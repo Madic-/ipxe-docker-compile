9ecc97ef6db5300cc9c60109e4673053d25e32d7
Madic- <3735459+Madic-@users.noreply.github.com>
2018-08-02 21:54:15 +0200

    Removed whitespace

--- build.sh
+++ build.sh
@@ -10,18 +10,18 @@ if [ ! -d "$DIR_WIMBOOT" ]; then mkdir -p "$DIR_WIMBOOT"; fi
 if [ ! -d "$DIR_LOGS" ]; then mkdir -p "$DIR_LOGS"; fi
 
 # Write log to file
-exec &> $DIR_LOGS/build.log
+exec &>$DIR_LOGS/build.log
 
 # Download and compile ipxe
 if [ -d $DIR_IPXE/.git ]; then
-  echo "Updating ipxe repository..."
-  cd $DIR_IPXE || exit
-  git pull
-  echo
+	echo "Updating ipxe repository..."
+	cd $DIR_IPXE || exit
+	git pull
+	echo
 else
-  echo "Cloning ipxe repository..."
-  git clone git://git.ipxe.org/ipxe.git $DIR_IPXE
-  echo
+	echo "Cloning ipxe repository..."
+	git clone git://git.ipxe.org/ipxe.git $DIR_IPXE
+	echo
 fi
 cp /opt/ipxe.local/* $DIR_IPXE/src/config/local/
 cd $DIR_IPXE/src || exit
